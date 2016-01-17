e9f949fb7dbcfb1b755d2c16831760d0a0a5201b
oeN <diomede.tripicchio@gmail.com>
2016-01-17 14:15:50 +0100

    edit method name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   before_create :generate_token
-  after_rollback :check_rollback
+  after_rollback :retry_token, on: :create
 
   attr_accessor :retry_count
 
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
     3
   end
 
-  def check_rollback
+  def retry_token
     @retry_count = (@retry_count || 0) + 1
     save if @retry_count <= retries_limit
   end
